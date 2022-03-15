5c88cb06afc05a1891008ddd602397297d7dd1ef
chi-mezie <chimezieuzoma36@gmail.com>
2022-03-14 20:40:00 -0700

    ased onne

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,33 @@
-#include <stdio.h>
-/**
-*
- *Return: 0 after printing the function
-*/
-int main(void)
-{puts("\"programming is like building a miltilingual puzzle");
-return 0;
-}
+#include <stdio.h>                                                                                                                                                                    
+
+                                                                                                                                                                                      
+
+/**                                                                                                                                                                                   
+
+                                                                                                                                                                                      
+
+ *main -entry point                                                                                                                                                                   
+
+                                                                                                                                                                                      
+
+ *                                                                                                                                                                                    
+
+                                                                                                                                                                                      
+
+ *Return: 0 after printing the function                                                                                                                                               
+
+                                                                                                                                                                                      
+
+*/                                                                                                                                                                                    
+
+                                                                                                                                                                                      
+
+int main(void)                                                                                                                                                                        
+
+                                                                                                                                                                                      
+
+{puts{"\"programming is like building a multilingual puzzle"};                                                                                                                        
+
+                                                                                                                                                                                      
+
+return 0;                                                                                                                                                                             
