6e0db4abb42acba1c03c0e0bb8172ec6456ee0f9
chi-mezie <chimezieuzoma36@gmail.com>
2022-03-14 21:05:19 -0700

    added 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,33 @@
+#include <stdio.h>                                                                                                                                                                    
+                                                                                                                                                                                      
+/**                                                                                                                                                                                   
+
+                                                                                                                                                                                      
+
+ *main -entry point                                                                                                                                                                   
+
+                                                                                                                                                                                      
+ *my hello, printf task
+
+                                                                                                                                                                                      
+
+ *Return: 0 after printing the function                                                                                                                                               
+
+                                                                                                                                                                                      
+
+*/                                                                                                                                                                                    
+
+                                                                                                                                                                                      
+
+int main(void)                                                                                                                                                                        
+
+                                                                                                                                                                                      
+
+{puts("with proper grammar, but the outcome was a piece of art.\n");                                                                                                                        
+
+                                                                                                                                                                                      
+
+return (0);
+}
+
+             
