85cfd7e772737a9c98c71b53194ad8cdf42055a3
chi-mezie <chimezieuzoma36@gmail.com>
2022-03-14 20:52:54 -0700

    new file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -26,8 +26,4 @@ int main(void)
 
                                                                                                                                                                                       
 
-{puts{"\"programming is like building a multilingual puzzle"};                                                                                                                        
-
-                                                                                                                                                                                      
-
-	return 0;                                                                                                                                                                      }
+{puts("\"programming is like building a multilingual puzzle");                                                                                                                         return (0);  }
