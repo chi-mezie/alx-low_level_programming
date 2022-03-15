1549515ba06fb9b4db1e0475ca5058bd92aa1cf3
chi-mezie <chimezieuzoma36@gmail.com>
2022-03-14 21:53:48 -0700

    last commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -26,11 +26,11 @@ int main(void)
 
                                                                                                                                                                                       
 
-{printf("size of a char: Id byte(s)\n", sizeof(char));
-printf("size of an int: Id byte(s)\n", sizeof(int));
-printf("size of a long int: Id byte(s)\n", sizeof(long int));
-printf("size of a long long : Id byte(s)\n", sizeof(long long int));
-printf("size of a float: Id bytes\n", sizeof(float));
+{printf("size of a char: 1 byte(s)\n", sizeof(char));
+printf("size of an int: 4 byte(s)\n", sizeof(int));
+printf("size of a long int: 4  byte(s)\n", sizeof(long int));
+printf("size of a long long : 8 byte(s)\n", sizeof(long long int));
+printf("size of a float: 4 bytes\n", sizeof(float));
 
 
                                                                                                                                                                                       
