bf7e69ae24ec42b34c5d0574051ae8b2757ef322
chi-mezie <chimezieuzoma36@gmail.com>
2022-03-14 21:49:52 -0700

    tired'

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -28,14 +28,14 @@ int main(void)
 
 {printf("size of a char: Id byte(s)\n", sizeof(char));
 printf("size of an int: Id byte(s)\n", sizeof(int));
-printf("size of a long int: Id byte(s)\n" sizeof(long int));
-printf("size of a long long : Id byte(s)\n" sizeof(long long int));
-printf("size of a float: Id bytes\n" sizeof(float));
+printf("size of a long int: Id byte(s)\n", sizeof(long int));
+printf("size of a long long : Id byte(s)\n", sizeof(long long int));
+printf("size of a float: Id bytes\n", sizeof(float));
 
 
                                                                                                                                                                                       
 
-return 0;                                                                                                                                                                             
+return (0);                                                                                                                                                 
 
              
 }
