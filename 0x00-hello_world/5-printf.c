d8077e77628530b855f5fb8f00ec481d670d0aff
chi-mezie <chimezieuzoma36@gmail.com>
2022-03-14 21:07:55 -0700

    added 5 agan

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -23,7 +23,7 @@ int main(void)
 
                                                                                                                                                                                       
 
-{puts("with proper grammar, but the outcome was a piece of art.\n");                                                                                                                        
+{puts("with proper grammar, but the outcome was a piece of art,\n");                                                                                                                        
 
                                                                                                                                                                                       
 
