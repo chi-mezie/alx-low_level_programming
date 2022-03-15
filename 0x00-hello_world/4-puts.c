32e1977fba26c360b12c0ce19e3cd0b32ed796bd
chi-mezie <chimezieuzoma36@gmail.com>
2022-03-14 20:25:08 -0700

    puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,9 @@
-#include <stdio.h>                                                                                                                                                                    
-
-/**                                                                                                                                                                                   
-
- *main -entry point                                                                                                                                                                   
-
- *                                                                                                                                                                                    
-
- *Return: 0 after printing the function                                                                                                                                               
-
-*/                                                                                                                                                                                    
-
-int main(void)                                                                                                                                                                        
-
-{puts{"\"programming is like building a multilingual puzzle"};                                                                                                                        
-
-return 0;                                                                                                                                                                             
-
-}  
+#include <stdio.h>
+/**
+*
+ *Return: 0 after printing the function
+*/
+int main(void)
+{puts("\"programming is like building a miltilingual puzzle");
+return 0;
+}
