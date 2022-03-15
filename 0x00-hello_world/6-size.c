7da50f60fe54402091090502d4856d59243978c7
chi-mezie <chimezieuzoma36@gmail.com>
2022-03-14 21:43:02 -0700

    size added

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,41 @@
+#include <stdio.h>                                                                                                                                                                    
+
+                                                                                                                                                                                      
+
+/**                                                                                                                                                                                   
+
+                                                                                                                                                                                      
+
+ *main -entry point                                                                                                                                                                   
+
+                                                                                                                                                                                      
+
+ *                                                                                                                                                                                    
+
+                                                                                                                                                                                      
+
+ *Return: 0 after printing the function                                                                                                                                               
+
+                                                                                                                                                                                      
+
+*/                                                                                                                                                                                    
+
+                                                                                                                                                                                      
+
+int main(void)                                                                                                                                                                        
+
+                                                                                                                                                                                      
+
+{printf("size of a char: Id byte(s)\n", sizeof(char));
+printf("size of an int: Id byte(s)\n", sizeof(int));
+printf("size of a long int: Id byte(s)\n" sizeof(long int));
+printf("size of a long long : Id byte(s)\n" sizeof(long long int));
+printf("size of a float: Id bytes\n" sizeof(float));
+
+
+                                                                                                                                                                                      
+
+return 0;                                                                                                                                                                             
+
+             
+}
