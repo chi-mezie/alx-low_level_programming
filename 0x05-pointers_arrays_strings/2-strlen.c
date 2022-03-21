34c6925ab32cbb0e2a25520bd36fd310eac7da04
mex-hunt <uzoma.chimezie@yahoo.com>
2022-03-21 17:24:41 +0100

    add again

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,12 +7,12 @@
  */
 int _strlen(char *s)
 {
-   int length = 0;
+ int length = 0;
 
-   while (*s != '\0')
-    {
-        s++;
-   	length++;
-    }
-   return (length);
+ while (*s != '\0')
+ {
+ s++;
+ length++;
+ }
+ return (length);
 }
